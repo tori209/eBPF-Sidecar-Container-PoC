4b35931c30e76c1200d0d25b7b86a95dac1abef0
tori209 <kyoungho2018@gist.ac.kr>
2025-04-28 05:59:29 +0000

    [fix] deploy 스크립트 명령 인식 오류 수정
    Deploy.sh 스크립트에서 명령 미입력 문제만 봤으나,
    실제 사용 시 명령 미입력 상태에서 기타 Param을 넘기면 apply로 인식되는
    문제 발견.
    이로 인해 명령 미입력 확인 대신, 유효한 명령인지 확인하는 로직으로 변경.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,16 +1,20 @@
 #! /usr/bin/env sh
-
 TEMPLATE_BASE=./config-template
+OPERATIONS="refresh apply rm nop" # 탐색 실패를 nop로 표기한다. 굳이 별도의 환경변수로 탐색 실패 여부 탐색 안해도 됨.
+HELP_MSG="[ERROR] USAGE: $0 [refresh/apply] [IMAGE_TAG(Mandatory)] [NAMESPACE(Optional; Default=default)] / rm [NAMESPACE]" 
+
 NAMESPACE=default
 TAG_NAME=
 OPTION=
 
-if [ -z $1 ]; then 
-	echo "[ERROR] USAGE: $0 [refresh/apply] [IMAGE_TAG(Mandatory)] [NAMESPACE(Optional; Default=default)] / rm [NAMESPACE]" > /dev/stderr
-	exit 1
-else
-	OPTION=$1
-fi
+for OP in $OPERATIONS; do
+	if [ $OP = "nop" ]; then
+		echo $HELP_MSG
+		exit 1
+	elif [ $OP = $1 ]; then
+		break
+	fi	
+done
 
 if [ $OPTION != "rm" ]; then
 	if [ -z $2 ]; then
